873a171c1b9f57a6382ab28783b17056dab91e62
Andrew Cincotta <cincotta.a@northeastern.edu>
2023-11-17 20:07:27 -0500

    Version 2.0! Added select-tab.tsx, a new command that allows the user to select a CopyQ tab. Completely reworked how clipboard contents are grabbed, resulting in a much faster loading time. With the rework of how clipboard contents are grabbed, preferences have been changed to allow the user to select how many clipboard entries to load.

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -7,7 +7,10 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {}
+type ExtensionPreferences = {
+  /** CopyQ Path - Path to CopyQ executable */
+  "copyq_path": string
+}
 
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences
@@ -15,18 +18,20 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `view-clipboard-entries` command */
   export type ViewClipboardEntries = ExtensionPreferences & {
-  /** CopyQ Path - Path to CopyQ executable */
-  "copyq_path": string,
   /** Default Tab - Name of Default CopyQ Tab */
   "default_tab": string,
-  /** Default Number of Items - Default Number of Items to Display */
-  "default_num_items": string
+  /** Max Tabs - Max number of CopyQ tabs to display. Set to 0 for no limit. Setting the limit too high will result in a long load time. */
+  "max_items": string
 }
+  /** Preferences accessible in the `select-tab` command */
+  export type SelectTab = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `view-clipboard-entries` command */
   export type ViewClipboardEntries = {}
+  /** Arguments passed to the `select-tab` command */
+  export type SelectTab = {}
 }
 
 
